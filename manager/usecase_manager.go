37a62f46ca080a63db1ec8ca1b99a74f1f7dacc8
dantri07 <dantri07sihombing19@gmail.com>
2023-08-09 15:54:53 +0700

    add : bootcamp repo,usecase,controller and pagination

--- manager/usecase_manager.go
+++ manager/usecase_manager.go
@@ -3,22 +3,26 @@ package manager
 import "interview_bootcamp/usecase"
 
 type UseCaseManager interface {
-    CandidateUseCase() usecase.CandidateUseCase
-    ResumeUseCase() usecase.ResumeUseCase
+	CandidateUseCase() usecase.CandidateUseCase
+	BootcampUseCase() usecase.BootcampUseCase
+	ResumeUseCase() usecase.ResumeUseCase
 }
 
 type useCaseManager struct {
-    repoManager RepoManager
+	repoManager RepoManager
 }
 
 func (u *useCaseManager) CandidateUseCase() usecase.CandidateUseCase {
-    return usecase.NewCandidateUseCase(u.repoManager.CandidateRepo())
+	return usecase.NewCandidateUseCase(u.repoManager.CandidateRepo())
+}
+func (u *useCaseManager) BootcampUseCase() usecase.BootcampUseCase {
+	return usecase.NewBootcampUseCase(u.repoManager.BootcampRepo())
 }
 
 func (u *useCaseManager) ResumeUseCase() usecase.ResumeUseCase {
-    return usecase.NewResumeUseCase(u.repoManager.ResumeRepo())
+	return usecase.NewResumeUseCase(u.repoManager.ResumeRepo())
 }
 
 func NewUseCaseManager(repoManager RepoManager) UseCaseManager {
-    return &useCaseManager{repoManager: repoManager}
+	return &useCaseManager{repoManager: repoManager}
 }

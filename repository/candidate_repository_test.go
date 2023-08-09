ff809264562e6aa69681bd766c0462845a7eea13
Albar Adimas Suntoro <albar230505@gmail.com>
2023-08-09 16:38:10 +0700

    wip: candidate repository testing

--- repository/candidate_repository_test.go
+++ repository/candidate_repository_test.go
@@ -131,6 +131,40 @@ func (suite *CandidateRepositoryTestSuite) TestGetCandidate_Fail() {
 	assert.Equal(suite.T(), model.Candidate{}, actualCandidate)
 }
 
+func (suite *CandidateRepositoryTestSuite) TestGetCandidateByPhoneNumber_Success() {
+	expectedCandidate := candidateDummy[0]
+	rows := sqlmock.NewRows([]string{"id", "full_name", "phone", "email", "date_of_birth", "address", "cv_link", "bootcamp_id", "bootcamp_name", "bootcamp_start", "bootcamp_end", "bootcamp_location", "instansi_pendidikan", "hackerrank_score"})
+	rows.AddRow(expectedCandidate.CandidateID, expectedCandidate.FullName, expectedCandidate.Phone, expectedCandidate.Email, expectedCandidate.DateOfBirth, expectedCandidate.Address, expectedCandidate.CvLink, expectedCandidate.Bootcamp.Id, expectedCandidate.Bootcamp.Name, expectedCandidate.Bootcamp.StartDate, expectedCandidate.Bootcamp.EndDate, expectedCandidate.Bootcamp.Location, expectedCandidate.InstansiPendidikan, expectedCandidate.HackerRank)
+	suite.mockSql.ExpectQuery(regexp.QuoteMeta("SELECT c.id,c.full_name,c.phone,c.email,c.date_of_birth,c.address,c.cv_link,b.id,b.name,b.start_date,b.end_date,b.location,c.instansi_pendidikan,c.hackerrank_score from candidate c INNER JOIN bootcamp b on b.id = c.bootcamp_id WHERE phone ILIKE $1")).WithArgs("%" + expectedCandidate.Email + "%").WillReturnRows(rows)
+	actualCandidate, actualError := suite.repo.GetByPhoneNumber(expectedCandidate.Email)
+	assert.NoError(suite.T(), actualError)
+	assert.Nil(suite.T(), actualError)
+	assert.Equal(suite.T(), expectedCandidate, actualCandidate)
+}
+func (suite *CandidateRepositoryTestSuite) TestGetCandidateByPhoneNumber_Fail() {
+	suite.mockSql.ExpectQuery(regexp.QuoteMeta("SELECT c.id,c.full_name,c.phone,c.email,c.date_of_birth,c.address,c.cv_link,b.id,b.name,b.start_date,b.end_date,b.location,c.instansi_pendidikan,c.hackerrank_score from candidate c INNER JOIN bootcamp b on b.id = c.bootcamp_id WHERE phone ILIKE $1")).WithArgs("sc1291").WillReturnError(fmt.Errorf("error"))
+	actualCandidate, err := suite.repo.GetByPhoneNumber("X1291")
+	assert.Error(suite.T(), err)
+	assert.Equal(suite.T(), model.Candidate{}, actualCandidate)
+}
+
+func (suite *CandidateRepositoryTestSuite) TestGetCandidateByEmail_Success() {
+	expectedCandidate := candidateDummy[0]
+	rows := sqlmock.NewRows([]string{"id", "full_name", "phone", "email", "date_of_birth", "address", "cv_link", "bootcamp_id", "bootcamp_name", "bootcamp_start", "bootcamp_end", "bootcamp_location", "instansi_pendidikan", "hackerrank_score"})
+	rows.AddRow(expectedCandidate.CandidateID, expectedCandidate.FullName, expectedCandidate.Phone, expectedCandidate.Email, expectedCandidate.DateOfBirth, expectedCandidate.Address, expectedCandidate.CvLink, expectedCandidate.Bootcamp.Id, expectedCandidate.Bootcamp.Name, expectedCandidate.Bootcamp.StartDate, expectedCandidate.Bootcamp.EndDate, expectedCandidate.Bootcamp.Location, expectedCandidate.InstansiPendidikan, expectedCandidate.HackerRank)
+	suite.mockSql.ExpectQuery(regexp.QuoteMeta("SELECT c.id,c.full_name,c.phone,c.email,c.date_of_birth,c.address,c.cv_link,b.id,b.name,b.start_date,b.end_date,b.location,c.instansi_pendidikan,c.hackerrank_score from candidate c INNER JOIN bootcamp b on b.id = c.bootcamp_id WHERE c.email ILIKE $1")).WithArgs("%" + expectedCandidate.Email + "%").WillReturnRows(rows)
+	actualCandidate, actualError := suite.repo.GetByEmail(expectedCandidate.Email)
+	assert.NoError(suite.T(), actualError)
+	assert.Nil(suite.T(), actualError)
+	assert.Equal(suite.T(), expectedCandidate, actualCandidate)
+}
+func (suite *CandidateRepositoryTestSuite) TestGetCandidateByEmail_Fail() {
+	suite.mockSql.ExpectQuery(regexp.QuoteMeta("SELECT c.id,c.full_name,c.phone,c.email,c.date_of_birth,c.address,c.cv_link,b.id,b.name,b.start_date,b.end_date,b.location,c.instansi_pendidikan,c.hackerrank_score from candidate c INNER JOIN bootcamp b on b.id = c.bootcamp_id where c.id = $1")).WithArgs("sc1291").WillReturnError(fmt.Errorf("error"))
+	actualCandidate, err := suite.repo.GetByEmail("X1291")
+	assert.Error(suite.T(), err)
+	assert.Equal(suite.T(), model.Candidate{}, actualCandidate)
+}
+
 func (suite *CandidateRepositoryTestSuite) TestDeleteCandidate_Success() {
 	suite.mockSql.ExpectExec("DELETE FROM candidate WHERE id=?").WithArgs(candidateDummy[0].CandidateID).WillReturnResult(sqlmock.NewResult(1, 1))
 	actualError := suite.repo.Delete(candidateDummy[0].CandidateID)
@@ -140,6 +174,20 @@ func (suite *CandidateRepositoryTestSuite) TestDeleteCandidate_Success() {
 func (suite *CandidateRepositoryTestSuite) TestDeleteCandidate_Fail() {
 	suite.mockSql.ExpectExec("DELETE FROM candidate WHERE id=?").WithArgs("1ABC").WillReturnError(fmt.Errorf("error"))
 	actualError := suite.repo.Delete("1ABC")
+	assert.Error(suite.T(), actualError)
+}
+
+func (suite *CandidateRepositoryTestSuite) TestUpdateCandidate_Success() {
+	dummy := candidateDummy[0]
+	suite.mockSql.ExpectExec(regexp.QuoteMeta("UPDATE candidate SET full_name = $2, phone = $3, email = $4, date_of_birth = $5, address = $6,cv_link = $7,bootcamp_id = $8,instansi_pendidikan= $9,hackerrank_score = $10  WHERE id = $1")).WithArgs(dummy.CandidateID, dummy.FullName, dummy.Phone, dummy.Email, dummy.DateOfBirth, dummy.Address, dummy.CvLink, dummy.Bootcamp.Id, dummy.InstansiPendidikan, dummy.HackerRank).WillReturnResult(sqlmock.NewResult(1, 1))
+	actualError := suite.repo.Update(dummy)
+	assert.Nil(suite.T(), actualError)
+}
+
+func (suite *CandidateRepositoryTestSuite) TestUpdateCandidate_Fail() {
+	dummy := candidateDummy[0]
+	suite.mockSql.ExpectExec(regexp.QuoteMeta("UPDATE candidate SET full_name = $2, phone = $3, email = $4, date_of_birth = $5, address = $6,cv_link = $7,bootcamp_id = $8,instansi_pendidikan= $9,hackerrank_score = $10  WHERE id = $1")).WithArgs(dummy.CandidateID, dummy.FullName, dummy.Phone, dummy.Email, dummy.DateOfBirth, dummy.Address, dummy.CvLink, dummy.Bootcamp.Id, dummy.InstansiPendidikan, dummy.HackerRank).WillReturnError(fmt.Errorf("error"))
+	actualError := suite.repo.Update(dummy)
 	assert.Error(suite.T(), actualError)
 }
 
@@ -160,15 +208,47 @@ func (suite *CandidateRepositoryTestSuite) TestPagingCandidate_Success() {
 	for _, candidate := range candidateDummy {
 		rows.AddRow(candidate.CandidateID, candidate.FullName, candidate.Phone, candidate.Email, candidate.DateOfBirth, candidate.Address, candidate.CvLink, candidate.Bootcamp.Id, candidate.Bootcamp.Name, candidate.Bootcamp.StartDate, candidate.Bootcamp.EndDate, candidate.Bootcamp.Location, candidate.InstansiPendidikan, candidate.HackerRank)
 	}
-	suite.mockSql.ExpectQuery("SELECT (.+) from candidate (.+)").WithArgs(paginationQuery.Take, paginationQuery.Skip).WillReturnRows(rows)
+	suite.mockSql.ExpectQuery(regexp.QuoteMeta("SELECT c.id,c.full_name,c.phone,c.email,c.date_of_birth,c.address,c.cv_link,b.id,b.name,b.start_date,b.end_date,b.location,c.instansi_pendidikan,c.hackerrank_score from candidate c INNER JOIN bootcamp b on b.id = c.bootcamp_id LIMIT $1 OFFSET $2")).WithArgs(paginationQuery.Take, paginationQuery.Skip).WillReturnRows(rows)
 
 	// COUNT
 	rowCount := sqlmock.NewRows([]string{"count"})
 	rowCount.AddRow(3)
 	suite.mockSql.ExpectQuery(regexp.QuoteMeta("SELECT COUNT(*) FROM candidate")).WillReturnRows(rowCount)
 
-	actualCandidate, actualPaging, actualError := suite.repo.Paging(requestPaging)
+	actualCandidates, actualPaging, actualError := suite.repo.Paging(requestPaging)
 	assert.Nil(suite.T(), actualError)
-	assert.NotNil(suite.T(), actualCandidate)
+	assert.NotNil(suite.T(), actualCandidates)
 	assert.Equal(suite.T(), actualPaging.TotalRows, 3)
+}
+
+func (suite *CandidateRepositoryTestSuite) TestPagingCandidat_QueryPagingError() {
+	suite.mockSql.ExpectQuery(regexp.QuoteMeta("SELECT c.id,c.full_name,c.phone,c.email,c.date_of_birth,c.address,c.cv_link,b.id,b.name,b.start_date,b.end_date,b.location,c.instansi_pendidikan,c.hackerrank_score from candidate c INNER JOIN bootcamp b on b.id = c.bootcamp_id LIMIT $1 OFFSET $2")).WillReturnError(fmt.Errorf("error"))
+	actualCandidates, actualPaging, actualError := suite.repo.Paging(dto.PaginationParam{})
+	assert.Error(suite.T(), actualError)
+	assert.Nil(suite.T(), actualCandidates)
+	assert.Equal(suite.T(), actualPaging.TotalRows, 0)
+}
+
+func (suite *CandidateRepositoryTestSuite) TestPagingCandidat_QueryCountError() {
+	requestPaging := dto.PaginationParam{
+		Page:  1,
+		Limit: 5,
+	}
+
+	paginationQuery := dto.PaginationQuery{
+		Take: 5,
+		Skip: 0,
+	}
+
+	rows := sqlmock.NewRows([]string{"id", "full_name", "phone", "email", "date_of_birth", "address", "cv_link", "bootcamp_id", "bootcamp_name", "bootcamp_start", "bootcamp_end", "bootcamp_location", "instansi_pendidikan", "hackerrank_score"})
+	for _, candidate := range candidateDummy {
+		rows.AddRow(candidate.CandidateID, candidate.FullName, candidate.Phone, candidate.Email, candidate.DateOfBirth, candidate.Address, candidate.CvLink, candidate.Bootcamp.Id, candidate.Bootcamp.Name, candidate.Bootcamp.StartDate, candidate.Bootcamp.EndDate, candidate.Bootcamp.Location, candidate.InstansiPendidikan, candidate.HackerRank)
+	}
+	suite.mockSql.ExpectQuery(regexp.QuoteMeta("SELECT c.id,c.full_name,c.phone,c.email,c.date_of_birth,c.address,c.cv_link,b.id,b.name,b.start_date,b.end_date,b.location,c.instansi_pendidikan,c.hackerrank_score from candidate c INNER JOIN bootcamp b on b.id = c.bootcamp_id LIMIT $1 OFFSET $2")).WithArgs(paginationQuery.Take, paginationQuery.Skip).WillReturnRows(rows)
+	rowCount := sqlmock.NewRows([]string{"count"})
+	rowCount.AddRow(3)
+	suite.mockSql.ExpectQuery(regexp.QuoteMeta("SELECT COUNT(*) FROM candidate")).WillReturnError(fmt.Errorf("error"))
+	_, actualPaging, actualError := suite.repo.Paging(requestPaging)
+	assert.Error(suite.T(), actualError)
+	assert.Equal(suite.T(), dto.Paging{}, actualPaging)
 }
